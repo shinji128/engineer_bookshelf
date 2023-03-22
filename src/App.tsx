41e50d854a846ac6cbe6b9a33907e43254e43c78
miura <shinji65536@gmail.com>
2023-03-22 12:02:19 +0900

    tailwind導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Booksearch } from './components/Booksearch';
 import { Login } from './components/Login';
+import './index.css'
 
 function App() {
   const [isAuth, setIsAuth] = useState<boolean>(localStorage.getItem('isAuth') === 'true');
