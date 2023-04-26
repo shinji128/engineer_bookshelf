4a3eebda76122011f991506a8bcf911c3a2b0db5
miura <shinji65536@gmail.com>
2023-04-26 15:07:07 +0900

    登録書籍表示

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import { useState } from 'react';
 import { SearchBooksPage } from './components/SearchBooksPage';
 import { Login } from './components/Login';
 import './index.css'
+import { MyBooksPage } from './components/MyBooksPage';
 
 function App() {
   const [isAuth, setIsAuth] = useState<boolean>(localStorage.getItem('isAuth') === 'true');
-
   return (
-    <>
+    <div className="bg-gray-100 p-5">
       <Login setIsAuth={setIsAuth}/>
       <SearchBooksPage />
-    </>
+      <MyBooksPage />
+    </div>
   );
 }
 
